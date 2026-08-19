439a539a7b66f1328787adfb6db878094e62ec23
kiss <41434771@qq.com>
2026-08-19 11:30:25 +0800

    feat: 编辑器基础布局骨架

---
+++ src/editor/index.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="editor h-screen">
+        <header class="h-56 header flex items-center px-20"></header>
+        <main class="editor-main flex">
+            <!-- material  -->
+            <aside class="w-256">material</aside>
+            <!-- layer -->
+            <aside class="w-156 ">layer</aside>
+            <!-- canvas -->
+            <div class="canvas flex-1">canvas</div>
+            <!-- propertery -->
+            <aside class="w-260 ">propertery</aside>
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+    name: 'ScreenEditor'
+})
+</script>
+
+<style scoped lang="scss">
+.editor {
+    background: var(--bg-color);
+
+    .header {
+        border-bottom: 1px solid var(--border-color);
+    }
+
+    .editor-main {
+        height: calc(100% - 56px);
+
+        .material,
+        .layer {
+            border-right: 1px solid var(--border-color)
+        }
+
+        .property {
+            border-left: 1px solid var(--border-color);
+        }
+    }
+}
+</style>
