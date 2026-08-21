6d5ffe69f1bafc2aac9284a50acfa21fb1c6a01d
kiss <41434771@qq.com>
2026-08-21 11:41:36 +0800

    feat: 接入echarts

---
+++ src/materials/charts/component.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="chart-material w-full h-full" ref="chart">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { MaterialSchema } from '@/schema/material';
+import { init,type EChartsType } from 'echarts'
+defineOptions({
+    name: 'ChartMaterial'
+})
+const props = defineProps<{ schema: MaterialSchema }>()
+const chartRef = useTemplateRef('chart')
+let chart:EChartsType
+onMounted(() => {
+    chart = init(chartRef.value)
+    chart.setOption(props.schema.props.option)
+
+    const observer = new ResizeObserver(() => {
+        chart.resize()
+
+    })
+
+    observer.observe(chartRef.value)
+
+    onBeforeUnmount(()=>{
+        observer.disconnect()
+        chart.dispose()
+    })
+})
+watch(()=>props.schema.props.option,()=>{
+    chart.setOption(props.schema.props.option)
+},{deep:true})
+</script>
+
+<style scoped lang="scss">
+.chart-material {}
+</style>
